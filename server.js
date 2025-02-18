5488c793b76dc4e156cfc9e4f8a52e247f58c350
Hany Shaker <hanyshaker@tanmia.local>
2025-02-18 14:33:17 +0200

    Updated API logic

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
     res.send("Subscribe to Arpan Neupane's channel");
 });
 
 app.listen(port, () => {
-    console.log("Server started on port 3000");
+    console.log("Server started on port 3001");
 });
